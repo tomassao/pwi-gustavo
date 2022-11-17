913db0829aefd402c9fea19cca72e469947c7d95
tomassao <ilovechrist@tuta.io>
2022-11-16 21:23:27 -0300

    Aula JS - 1

---
+++ Aula07-JS/console-typeof.js
@@ -0,0 +1,21 @@
+var variavel1 = {
+    nome = "Jorge", idade = 2
+}
+
+var variavel2 = "2"
+
+console.log(typeof variavel2)
+console.log(typeof variavel1.idade)
+
+//console.log(Number.isInteger(variavel1))
+//console.log(variavel1.toString(16)) - pode converter para binário ou decimal; para ser string os pararênteses precisam estar vazios
+//console.log(variavel1.toFixed) - arredonda números p/ exemplo: console.log(variavel.toFixed(3))
+//console.log(typeof variavel1) 
+//variavel.charCodeAt(2) - retorna um carácter baseado em sua posição/índice
+//variavel.indexOf(J) - retorna um índice baseado em um carácter
+// .subString(0) - retira uma letra de uma String
+// .subString(0,3) - retorna apenas as letras baseado no índice
+// 'texto1'.concat(variavel).concat('!') - con
+//variavel.replace(3, e) - o que está na posição 3 será substituido pela letra 'e'
+// console.log(variavel.replae(/\d/, 'e')) 
+//Extensão Code Runner
---
+++ exercicios-js/fundamentos/numerosAlgunsCuidados.js
@@ -0,0 +1,9 @@
+console.log(7 / 0) // resulta em infinity
+console.log("10" / 2) // retorna resultado pois elee verifica se a string possui um valor númerico. 10,2 não funciona
+console.log("Show" * 2) // NaN = Not a Number
+console.log(0.1 + 0.7) // possui algumas imprecisões
+// console.log(10.toString())
+console.log((10.345).toFixed(2))
+console.log("3"+1) // ele concatena pois o + faz sentido no contexto de uma string
+console.log("3"-1) // ele soma pois o - não faz sentido no contexto da string
+
